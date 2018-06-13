6945488c30679077443a54a0f7f7fd4306aa2c93
JasonEtco <jasonetco@gmail.com>
2018-06-13 11:32:38 -0400

    Formatting

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const github = require('@octokit/rest')({
   headers: {
     accept: 'application/vnd.github.hellcat-preview+json'
   },
-  //Set this to GHE url
-  baseUrl: ''
+  // Set this to GHE url
+  baseUrl: process.env.GHE_URL || ''
 })
 require('./pagination')(github)
 
@@ -16,34 +16,34 @@ github.authenticate({
   token: process.env.ghToken
 })
 
-var table = []
+let table = []
 
-async function getData() {
+async function getData () {
   const orgs = [].concat.apply([], (await github.paginate(github.orgs.getAll())).map(d => d.data.map(n => n.login)))
   const members = [].concat.apply([], (await github.paginate(github.users.getAll())).map(d => d.data.map(n => n.login)))
 
   for (const org of orgs) {
 
-    //Get all repositories for the organization
-    var repos = [].concat.apply([], (await github.paginate(github.repos.getForOrg({
+    // Get all repositories for the organization
+    const repos = [].concat.apply([], (await github.paginate(github.repos.getForOrg({
       org: org
     }))).map(d => d.data.map(r => r)))
 
     for (const repo of repos) {
-      //Pull a list of teams and their access to the current repository
+      // Pull a list of teams and their access to the current repository
       const repoTeams = [].concat.apply([], (await github.paginate(github.repos.getTeams({
         owner: org,
         repo: repo.name
       }))).map(d => d.data.map(t => t)))
 
-      //Pull a list of outside collaborators for the current repository
+      // Pull a list of outside collaborators for the current repository
       const repoCollabs = [].concat.apply([], (await github.paginate(github.repos.getCollaborators({
         owner: org,
         repo: repo.name,
         affiliation: 'outside'
       }))).map(d => d.data.map(c => c)))
 
-      //Loop teams and query permissions and members
+      // Loop teams and query permissions and members
       for (const team of repoTeams) {
         const memberData = await github.paginate(github.orgs.getTeamMembers({
           id: team.id
@@ -76,9 +76,8 @@ async function getData() {
     }
   }
 
-  //get member repositories
+  // Get member repositories
   for (const member of members) {
-
     const memberRepos = await github.paginate(github.repos.getForUser({
       username: member,
       type: 'all'
@@ -99,29 +98,26 @@ async function getData() {
   }
 }
 
-getData().then(function() {
-  //Remove Duplicates
+getData().then(() => {
+  // Remove Duplicates
   table = table.filter((table, index, self) =>
     index === self.findIndex((t) => (
       t.repo === table.repo && t.user === table.user && t.team === table.team
     ))
   )
 
-  //Sort by Team 
-  table.sort(function (a, b) {
+  // Sort by Team 
+  table.sort((a, b) => {
     return a.repo > b.repo ? 1 : b.repo > a.repo ? -1 : 0;
   });
 
-  //Write to CSV file
-  var jsonResults = JSON.stringify(table)
+  // Write to CSV file
   const fields = ['org', 'repo', 'team', 'user', 'permission', 'type']
-  var json2csvParser = new Json2csvParser({
-    fields,
-    delimiter: ';'
-  })
+  const json2csvParser = new Json2csvParser({ fields, delimiter: ';' })
+
   const csv = json2csvParser.parse(table)
   console.log(csv)
-  fs.writeFile('repo-permissions.csv', csv, function (err) {
+  fs.writeFile('repo-permissions.csv', csv, (err) => {
     if (err) throw err
     console.log('file saved!')
   })
